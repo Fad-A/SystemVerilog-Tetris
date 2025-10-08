5c396f2b2b4aa9cc6ff06372ea6977290cc45594
Fad-A <fadi.joseph100@yahoo.com>
2025-10-08 09:11:02 -0400

    Added other directional move modules

--- MoveModules.sv
+++ MoveModules.sv
@@ -1,28 +1,95 @@
 typedef struct packed {
-  logic [2:0][9:0] shape;
+  logic [3:0][9:0] shape;
   logic [4:0] shapeRowPos; 
-} shape;
+} shapeStruct;
 
 
-// Take the shape and move it to the right on the screen. Checks if it can even do that
-module moveRight(input shape shp, input logic [19:0][9:0] screen, input logic clk,
-                 output logic [19:0][9:0] outputScreen);
-    always_comb begin
+// Take the shape and move it to the right on the screen.
+module moveRight(input shapeStruct shape, input logic [19:0][9:0] screen, input logic clk, moveSignal,
+                 output logic [19:0][9:0] outputScreen, output shapeStruct outputShape);
     logic atEdge;
-    logic [2:0][9:0] shape_shifted = shp.shape;
-    assign atEdge = shp.shape[0][0] | shp.shape[1][0] | shp.shape[2][0];
-    screen[shp.shapeRowPos:shp.shapeRowPos-2] = screen[shp.shapeRowPos:shp.shapeRowPos-2] ^ shp.shape;
-    end
-
-    always_ff @(posedge clk) begin
-        if (atEdge) begin
-            outputScreen = screen;
+    shapeStruct [3:0][9:0] shape_shifted;
+    assign shape_shifted.shapeRowPos = shape.shapeRowPos;
+    logic [19:0][9:0] nextOutput
+    always_comb
+    begin  
+        atEdge = shape.shape[0][0] | shape.shape[1][0] | shape.shape[2][0] | shape.shape[3][0];
+        nextOutput = screen 
+        nextOutput[shape.shapeRowPos +:4] = nextOutput[shape.shapeRowPos +:4] ^ shape.shape;
+        for (int r = 0; r < 4; r++) begin
+            shape_shifted.shape[r] = {1'b0, shape.shape[r][9:1]};
         end
-        else begin
-            for (int r = 0; r < 3; r++) begin
-                shape_shifted[r] = {1'b0, shp.shape[r][8:0]};
-            end
-            outputScreen[shp.shapeRowPos:shp.shapeRowPos-2] = screen[shp.shapeRowPos:shp.shapeRowPos-2] | shape_shifted;
+        nextOutput[shape.shapeRowPos +:4] = nextOutput[shape.shapeRowPos +:4] | shape_shifted.shape;
+    end
+    always_ff @(posedge clk) begin
+        if(!moveSignal | atEdge) begin
+            output <= screen;
+            outputShape <= shape.shape;
+        end 
+        else
+        begin
+            output <= nextOutput;
+            outputShape <= shape_shifted;
+        end
+    end
+endmodule 
+
+// Take the shape and move it to the left on the screen.
+module moveLeft(input shapeStruct shape, input logic [19:0][9:0] screen, input logic clk, moveSignal,
+                 output logic [19:0][9:0] outputScreen, output shapeStruct outputShape);
+    logic atEdge;
+    shapeStruct [3:0][9:0] shape_shifted;
+    assign shape_shifted.shapeRowPos = shape.shapeRowPos;
+    logic [19:0][9:0] nextOutput
+    always_comb
+    begin  
+        atEdge = shape.shape[0][9] | shape.shape[1][9] | shape.shape[2][9] | shape.shape[3][9];
+        nextOutput = screen 
+        nextOutput[shape.shapeRowPos +:4] = nextOutput[shape.shapeRowPos +:4] ^ shape.shape
+        for (int r = 0; r < 4; r++) begin
+            shape_shifted.shape[r] = {shape.shape[r][8:0] , 1'b0};
+        end
+        nextOutput[shape.shapeRowPos +:4] = nextOutput[shape.shapeRowPos +:4] | shape_shifted.shape;
+    end
+    always_ff @(posedge clk) begin
+        if(!moveSignal | atEdge) begin
+            output <= screen;
+            outputShape <= shape.shape;
+        end 
+        else
+        begin
+            output <= nextOutput;
+            outputShape <= shape_shifted;
+        end
+    end
+endmodule
+
+// Take the shape and move it to the down on the screen.
+module moveDown(input shapeStruct shape, input logic [19:0][9:0] screen, input logic clk, moveSignal,
+                 output logic [19:0][9:0] outputScreen, output shapeStruct outputShape);
+    logic atEdge;
+    logic [19:0][9:0] nextOutput
+    always_comb
+    begin  
+        atEdge = shape.shapeRowPos == 0;
+        if (!atEdge) begin
+            nextOutput = screen 
+            nextOutput[shape.shapeRowPos +:4] = nextOutput[shape.shapeRowPos +:4] ^ shape.shape; // Remove current shape from 
+            nextOutput[shape.shapeRowPos +:4] = nextOutput[shape.shapeRowPos-1 +:4] | shape.shape; // add it back 
+        end
+        
+    end
+    always_ff @(posedge clk) begin
+        if(!moveSignal | atEdge) begin
+            output <= screen;
+            outputShape <= shape;
+        end 
+        else
+        begin
+            output <= nextOutput;
+            outputShape.shape <= shape.shape;
+            outputShape.shapeRowPos <= shape.shapeRowPos - 1;
+            
         end
     end
 endmodule 
